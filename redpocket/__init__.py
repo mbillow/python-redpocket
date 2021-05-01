a5e3f4aac64e5df969a2260e7309fc1a585d255a
Marc Billow <mbillow@me.com>
2021-05-01 16:00:35 -0500

    Exclude unused imports for init

--- redpocket/__init__.py
+++ redpocket/__init__.py
@@ -4,11 +4,11 @@ Author: Marc Billow
 License: MIT
 """
 from .api import RedPocket  # noqa: F401
-from .exceptions import (
+from .exceptions import (  # noqa: F401
     RedPocketException,
     RedPocketAuthError,
     RedPocketAPIError,
-)  # noqa: F401
+)
 
 
 __version__ = "0.1.1"
